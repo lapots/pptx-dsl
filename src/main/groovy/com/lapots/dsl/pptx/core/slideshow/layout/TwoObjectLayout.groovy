095420c7a109b15aa56dcd4c453672c004503c9d
lapots <sebastrident@gmail.com>
2017-12-12 22:47:21 +0300

    Experimenting with shapes

--- src/main/groovy/com/lapots/dsl/pptx/core/slideshow/layout/TwoObjectLayout.groovy
+++ src/main/groovy/com/lapots/dsl/pptx/core/slideshow/layout/TwoObjectLayout.groovy
@@ -3,8 +3,16 @@ package com.lapots.dsl.pptx.core.slideshow.layout
 import com.lapots.dsl.pptx.core.CommonDelegateTrait
 import com.lapots.dsl.pptx.core.slideshow.layout.util.LayoutSupportUtils
 import com.lapots.dsl.pptx.core.slideshow.text.SlideshowText
+import org.apache.poi.sl.draw.SLGraphics
 import org.apache.poi.xslf.usermodel.XMLSlideShow
+import org.apache.poi.xslf.usermodel.XSLFDrawing
+import org.apache.poi.xslf.usermodel.XSLFGroupShape
+import org.apache.poi.xslf.usermodel.XSLFShape
 import org.apache.poi.xslf.usermodel.XSLFSlide
+
+import java.awt.Graphics2D
+import java.awt.Rectangle
+import java.awt.geom.Rectangle2D
 
 /**
  * Handles TWO_OBJ layout.
@@ -42,5 +50,15 @@ class TwoObjectLayout implements CommonDelegateTrait {
     def image(closure) {
         def img = closure() as String
         LayoutSupportUtils.addPicture(img, ppt, pptSlide, contentOffset)
+
+        // investigate
+        XSLFGroupShape groupShape = pptSlide.createGroup()
+        groupShape.clear()
+        Rectangle2D rectangle2D = new Rectangle()
+        rectangle2D.setBounds(0, 0, 20, 20)
+
+        Graphics2D graphics2D = new SLGraphics(groupShape)
+        graphics2D.drawRect(0, 0, 20, 20)
+        groupShape.setAnchor(rectangle2D)
     }
 }
